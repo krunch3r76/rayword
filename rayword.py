044d4d73b35a1549f94c972bfb2f8ee35459c544
krunch3r76 <biz@u26a4.com>
2024-08-26 10:43:57 -0700

    fix logging

--- rayword.py
+++ rayword.py
@@ -8,9 +8,10 @@ from controller import Controller
 # Explicitly get the root logger
 root_logger = logging.getLogger()
 root_logger.setLevel(logging.DEBUG)
+root_logger.handlers = []
 
 # Create a file handler to write logs to a file
-file_handler = logging.FileHandler("wtf.log", mode="w")
+file_handler = logging.FileHandler("debug.log", mode="w")
 file_handler.setLevel(logging.DEBUG)
 
 # Create a formatter and set it for the handler
@@ -19,11 +20,6 @@ file_handler.setFormatter(formatter)
 
 # Add the handler to the root logger
 root_logger.addHandler(file_handler)
-
-# Optionally remove other handlers (like the default stream handler)
-for handler in root_logger.handlers:
-    if isinstance(handler, logging.StreamHandler):
-        root_logger.removeHandler(handler)
 
 try:
     controller = Controller()
